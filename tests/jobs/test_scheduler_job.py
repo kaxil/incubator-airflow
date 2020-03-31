apache/spark

[AIRFLOW-6802] Fix bug where dag.max_active_run wasn't always honored by scheduler (#7416)
commit 50efda5c69c1ddfaa869b408540182fb19f1a286 introduced a bug that
prevents the scheduler from enforcing max active run config for all
DAGs.

this commit fixes the regression as well as the test.

tests/jobs/test_scheduler_job.py
@@ -740,7 +740,10 @@ class TestDagFileProcessor(unittest.TestCase):
         dag.clear()
 
         # First create up to 3 dagruns in RUNNING state.
-        dag_file_processor.create_dag_run(dag)
+        assert dag_file_processor.create_dag_run(dag) is not None
+        assert dag_file_processor.create_dag_run(dag) is not None
+        assert dag_file_processor.create_dag_run(dag) is not None
+        assert len(DagRun.find(dag_id=dag.dag_id, state=State.RUNNING, session=session)) == 3
 
         # Reduce max_active_runs to 1
         dag.max_active_runs = 1

kaxil/incubator-airflow

(cherry picked from commit 343d38af380afad2b202838317a47a7b1687f14f)

tests/jobs/test_scheduler_job.py
@@ -1693,7 +1693,10 @@ class SchedulerJobTest(unittest.TestCase):
         dag.clear()
 
         # First create up to 3 dagruns in RUNNING state.
-        scheduler.create_dag_run(dag)
+        assert scheduler.create_dag_run(dag) is not None
+        assert scheduler.create_dag_run(dag) is not None
+        assert scheduler.create_dag_run(dag) is not None
+        assert len(DagRun.find(dag_id=dag.dag_id, state=State.RUNNING, session=session)) == 3
 
         # Reduce max_active_runs to 1
         dag.max_active_runs = 1